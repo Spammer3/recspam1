82a355fb32b4848185650d876310478b636fe64d
Spammer3 <48310590+Spammer3@users.noreply.github.com>
2019-03-07 09:10:00 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("https://discord.gg/BzrtKU")
+setInterval(function() {
+channel.send(`ProSpammer`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
